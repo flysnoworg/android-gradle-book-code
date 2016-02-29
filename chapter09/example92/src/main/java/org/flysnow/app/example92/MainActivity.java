b22666663bc9d4c3e2cc9fc3bf045867662d40f4
飞雪无情 <ls8707@gmail.com>
2016-02-29 23:28:45 +0800

    增加第九章第二小节的示例代码

---
+++ chapter09/example92/src/main/java/org/flysnow/app/example92/MainActivity.java
@@ -0,0 +1,27 @@
+package org.flysnow.app.example92;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] sharedLibraryNames = getPackageManager().getSystemSharedLibraryNames();
+        StringBuilder resultBuild = new StringBuilder();
+        for(String name:sharedLibraryNames){
+            resultBuild.append(name);
+            resultBuild.append("\n");
+        }
+        ((TextView)findViewById(android.R.id.text1)).setText(resultBuild);
+        HttpClient  httpClient = new DefaultHttpClient();
+    }
+
+}
