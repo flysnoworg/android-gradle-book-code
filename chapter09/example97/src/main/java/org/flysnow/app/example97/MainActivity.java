ae991f4abe505d04a7ea88911deb82ac26033c6c
飞雪无情 <ls8707@gmail.com>
2016-03-19 22:53:11 +0800

    添加自定义资源示例代码

---
+++ chapter09/example97/src/main/java/org/flysnow/app/example97/MainActivity.java
@@ -0,0 +1,16 @@
+package org.flysnow.app.example97;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
