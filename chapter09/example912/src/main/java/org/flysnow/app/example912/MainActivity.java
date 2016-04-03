1ffad6e3baa2871824e969636a5d80d1fee5b888
飞雪无情 <ls8707@gmail.com>
2016-04-04 00:01:19 +0800

    添加自动清理无用资源一节示例代码

---
+++ chapter09/example912/src/main/java/org/flysnow/app/example912/MainActivity.java
@@ -0,0 +1,16 @@
+package org.flysnow.app.example912;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getResources().getIdentifier("unused","drawable",getPackageName());
+    }
+
+}
