2838bd231b496c652f13804de765fa07ff36d6c0
飞雪无情 <ls8707@gmail.com>
2016-04-24 22:43:34 +0800

    增加Flurry多渠道演示示例代码

---
+++ chapter11/example112/src/main/java/org/flysnow/app/example112/MainActivity.java
@@ -0,0 +1,15 @@
+package org.flysnow.app.example112;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
